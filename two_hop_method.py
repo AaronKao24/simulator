47f9eef61902997df214c0884d3062885e28f694
AaronKao24 <jinjus14789632@gmail.com>
2021-03-12 22:10:50 +0800

    2021/3/12 Update

--- two_hop_method.py
+++ two_hop_method.py
@@ -1,16 +1,26 @@
+import parameter
+from operator import itemgetter
+from math import hypot , ceil
 
 vec_per = []
 vec_all = {}
+vec_back_list = []
+for x in range(parameter.vec_num*2):    #初始化list
+    vec_back_list.insert(x , [])        #初始化後方車輛list
+    
+
+
 def main(vec , time):
+    
     global vec_all
     global vec_per
     vec_all = vec
     add_vec_info()
-    if time % 1000 == 0 :
-        for x in vec_per:
-            print(x["id"] ," : " , x["direction"])
+    get_back_vec(time)
     vec_per = [] 
-    return 1
+
+
+    return 0
 
 def add_vec_info():
     global vec_per
@@ -38,3 +48,44 @@ def add_vec_info():
                                 #  "select_time" : select_time_list[int(x)],
                                  
                                 })
+
+def get_back_vec(time):
+    for x in range(parameter.vec_num*2):    #初始化list
+        vec_back_list.insert(x , [])        #初始化後方車輛list
+    global get_back_vec
+
+    for x in vec_per:
+        for y in vec_per:
+            if x["ypos"] > 2000:    #車輛y座標大於y軸高度
+                if y["direction"] == x["direction"]:    #判斷同方向
+                    if x["direction"] == "forward":     #若正向 則 小於比較對象為先行車
+                        if x["xpos"] < y ["xpos"]:
+                            vec_back_list[x["id"]].insert(y["id"] , {
+                                "id" : y["id"],
+                                "dis" : hypot(x["xpos"] - y["xpos"] , x["ypos"] - y["ypos"])
+                            })
+                    else:
+                        if x["xpos"] > y["xpos"]:
+                            vec_back_list[x["id"]].insert(y["id"] , {
+                                "id" : y["id"],
+                                "dis" : hypot(x["xpos"] - y["xpos"] , x["ypos"] - y["ypos"])
+                            })
+            elif x["ypos"] < 2000:  #車輛y座標小於y軸高度
+                if x["direction"] == "reserve":
+                    if y["direction"] == x["direction"]:    #判斷同方向
+                        if x["xpos"] < y ["xpos"]:
+                            vec_back_list[x["id"]].insert(y["id"] , {
+                                    "id" : y["id"],
+                                    "dis" : hypot(x["xpos"] - y["xpos"] , x["ypos"] - y["ypos"])
+                                })
+                        else:
+                            if x["xpos"] > y["xpos"]:
+                                vec_back_list[x["id"]].insert(y["id"] , {
+                                    "id" : y["id"],
+                                    "dis" : hypot(x["xpos"] - y["xpos"] , x["ypos"] - y["ypos"])
+                                })
+                                
+        if len(vec_back_list[x["id"]]) > 5: 
+            vec_back_list[x["id"]] = sorted(vec_back_list[x["id"]] , key=itemgetter("dis"))
+            if time %1000 ==0:
+                print(x["id"]," : ",vec_back_list[x["id"]])
